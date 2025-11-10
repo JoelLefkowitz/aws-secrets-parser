2c9c6663b3381d7b8af054ade684723a5362bd63
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-11-10 11:18:12 +0000

    Add CLI parser

---
+++ src/cli.ts
@@ -0,0 +1,48 @@
+import { FormatOptions } from "./formatters";
+import { applySpec, prop } from "ramda";
+import { description } from "../package.json";
+import yargs from "yargs";
+
+export interface CLI {
+  name: string;
+  region: string;
+  options: FormatOptions;
+}
+
+export const cli = (argv: string[]) =>
+  applySpec<CLI>({
+    name: prop("name"),
+    region: prop("region"),
+    options: {
+      format: prop("format"),
+      prefix: prop("prefix"),
+    },
+  })(
+    yargs(argv)
+      .command("$0 <name>", description, (yargs) =>
+        yargs
+          .positional("name", {
+            type: "string",
+            demandOption: true,
+          })
+          .option("region", {
+            type: "string",
+            alias: "r",
+            default: "us-east-1",
+          })
+          .option("format", {
+            type: "string",
+            alias: "f",
+            choices: ["constant", "pascal"],
+          })
+          .option("prefix", {
+            type: "string",
+            alias: "p",
+          }),
+      )
+      .help()
+      .version()
+      .alias("help", "h")
+      .alias("version", "v")
+      .parseSync(),
+  );
